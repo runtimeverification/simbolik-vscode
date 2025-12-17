44bd7bce5a4b739ed537a39a8901abd74b5df464
Yiyi Wang <yiyi.wang@runtimeverification.com>
2025-12-17 11:01:38 +0800

    feat: Add github action to test

--- shell.nix
+++ shell.nix
@@ -4,7 +4,7 @@
 with pkgs;
 mkShell rec {
   buildInputs = [
-    nodejs_22 # Follow the vscode node version requirement
+    nodejs_20 # Follow the vscode node version requirement
   ];
   LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
   LANG = "C.UTF-8";
